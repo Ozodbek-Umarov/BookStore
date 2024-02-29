bd3806e2f008c5a63c68b1f60ab239a71807df44
Saidjon <saidjonnozimboyevv@gmail.com>
2024-02-29 17:33:37 +0500

    .

--- Book/Data/Entities/Books.cs
+++ Book/Data/Entities/Books.cs
@@ -8,6 +8,7 @@ public class Books : BaseEntity
     public int AuthorId { get; set; }
     public Author Author { get; set; } = new Author();
     public int GenreId { get; set; }
+
     public Genre Genre { get; set; } = new Genre();
     public ICollection<OrderItems> OrderItems { get; set; } = new List<OrderItems>();
 }
