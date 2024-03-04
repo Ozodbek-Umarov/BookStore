a6e7b6b53a7ed2a83bc5160b6ab8e95cde04edbf
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-03-04 15:51:06 +0500

    ......

--- Book/BusinessLogic/Common/PageModel.cs
+++ Book/BusinessLogic/Common/PageModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Book.BusinessLogic.Common
+﻿namespace Book.BusinessLogic.Common;
+
+public class PageModel<T>
 {
-    public class PageModel
-    {
-    }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalItemsCount { get; set; }
+    public List<T> Items { get; set; } = new();
 }
