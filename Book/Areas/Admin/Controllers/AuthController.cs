7b33da49141fbbcd3d0bda95e4e8e2cc21e1e545
Saidjon <saidjonnozimboyevv@gmail.com>
2024-03-07 14:02:17 +0500

    one controller added

---
+++ Book/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Book.BusinessLogic.DTOs.UserDTOs;
+using Book.BusinessLogic.Interfaces;
+using Book.BusinessLogic.Services;
+using Book.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Areas.Admin.Controllers;
+
+[Area("admin")]
+
+public class AuthController(IAuthService authService)
+        : Controller
+{
+    private readonly IAuthService authService = authService;
+
+    public IActionResult Login()
+    {
+        LoginDto dto = new();
+        return View(dto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginDto dto)
+    {
+        var result = await authService.LoginAsync(dto, Role.Admin);
+        if (result.IsSuccess)
+        {
+            return RedirectToAction("index", "home");
+        }
+
+        dto.Error = result.ErrorMessage;
+        return View(dto);
+    }
+
+    public IActionResult Logout()
+    {
+        authService.Logout(Role.Admin);
+        return RedirectToAction("login");
+    }
+
+}
+
